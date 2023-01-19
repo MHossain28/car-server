60b752067a968d8291807e6fd3451fe7d0cfcb2d
MHossain <mhh52728@gmail.com>
2023-01-19 20:04:16 +0600

    basic node mongo setup

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middle wares
+app.use(cors());
+app.use(express.json());
+
+// Cunect MongoDB
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.td9wdol.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+app.get("/", (req, res) => {
+  res.send("car servicing server is running");
+});
+
+app.listen(port, () => {
+  console.log(`Car Servicing server running on ${port}`);
+});
